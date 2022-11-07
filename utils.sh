5adf97da0fd08e45dbe8984559b91cfe766b9b7b
DJ Adams <dj.adams@sap.com>
2022-11-07 17:04:19 +0000

    start to collect urls

---
+++ utils.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+de_entify() {
+
+  local str
+  read -r str < /dev/stdin
+  echo "$str" \
+  | sed '
+    s/&#8211;/-/;
+    s/&#8230;/.../;
+    s/&lt;/</;
+    s/&gt;/>/;
+    s/:/-/;
+    '
+
+}
+
+get_title() {
+
+  local url="${1:?Specify URL}"
+  curl \
+    --silent \
+    --url "$url" \
+    | grep \
+      --perl-regexp \
+      --only-matching \
+      "(?<=<title>)(.+)(?= \| SAP Blogs)" \
+    | de_entify
+
+}
+
+
+get_postdate() {
+
+  # Pick out the yyyy mm and dd values from the URL and return
+  # a yyyy-mm-dd postdate string.
+  local url=$1
+  echo "$url" \
+    | sed -E 's|.+\.com/([0-9]{4})/([0-9]{2})/([0-9]{2})/.+|\1-\2-\3|'
+
+}
